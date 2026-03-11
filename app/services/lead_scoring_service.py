252f472d286d2ac2e61d963745525028dc7a6843
Debasmita Chatterjee <115386608+debasmita30@users.noreply.github.com>
2026-03-12 01:29:04 +0530

    Update lead_scoring_service.py

--- app/services/lead_scoring_service.py
+++ app/services/lead_scoring_service.py
@@ -1,14 +1,13 @@
+import warnings
+warnings.filterwarnings("ignore")
 import joblib
 import numpy as np
 
 model = joblib.load("app/ml/lead_model.pkl")
 
 def predict_lead_score(company_size, budget, urgency, ai_interest):
-
     features = np.array([
         [company_size, budget, urgency, ai_interest]
     ])
-
     score = model.predict(features)[0]
-
     return round(score, 2)
